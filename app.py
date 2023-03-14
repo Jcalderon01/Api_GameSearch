8e7876812556a96cb41761a7dba34875fc92d41a
Jorge Alejandro Calderón Ruiz <104863690+Jcalderon01@users.noreply.github.com>
2023-03-13 18:16:45 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,39 @@
-from flask import Flask
+import json
+from flask import Flask, Response
 from flask import jsonify
 
 app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def game_search():
+    data = {
+        "firstName": "GameSearch",
+        "descripcion": "Es un servicio especializado en recomendarte tus juegos preferidos",
+        "url": "GameSearch.com",
+        "precio": "free",
+        "endpoints": [
+            {
+                "url": "/api/profile/recomendations",
+                "limit": 3
+            },
+            {
+                "url": "/api/game/price",
+                "limit": 5
+            },
+            {
+                "url": "/api",
+                "limit": 15
+            }
+        ]
+    }
+    response = Response(response=json.dumps(
+        data), status=200, mimetype="application/json")
+    return response
+
+
+if __name__ == 'main':
+    app.run(debug=True)
 
 
 @app.route("/")
