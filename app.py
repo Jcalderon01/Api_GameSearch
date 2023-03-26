1a450d3e4dcdecffc827456fd5e9b5cbfe2b7703
Jorge Alejandro Calderón Ruiz <104863690+Jcalderon01@users.noreply.github.com>
2023-03-26 13:51:42 -0700

    Update app.py

--- app.py
+++ app.py
@@ -51,3 +51,31 @@ def about():
     with open('api.json', "r") as json:
         display = json.read()
         return display
+    
+    
+@app.route("/api/sql/videogames")
+def videogames():
+    connection = mysql.connector.connect(host='localhost',
+                                         database='gamesearch',
+                                         user='',
+                                         password='')
+    
+    sql_select_Query = "select * from videogames"
+    cursor = connection.cursor()
+    cursor.execute(sql_select_Query)
+    records = cursor.fetchall()
+    datos_json = []
+    for row in records:
+        datos_json.append({
+            'videogame_id': row[0], 
+            'videogame_name': row[1],
+            'videogame_publisher': row[2],
+            'videogame_release_date': row[3],
+            'videogame_platform': row[4],
+            'videogame_genre': row[5],
+            'videogame_rating': row[6],
+            'videogame_description': row[7]
+        })
+        
+    connection.close()
+    return jsonify(datos_json)
